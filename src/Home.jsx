41005b61c02f44d798b09ccc5525efc46874de47
fahrizak <webdaata@gmail.com>
2024-02-13 15:42:19 +0700

    handdling fetch error

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,14 +5,26 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, SetBlogs] = useState(null)
     const [isPending, SetIsPending] = useState(true)
+    const [error, SetError] = useState(null)
 
     useEffect(() => {
         
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-            .then(res => res.json())
+            .then(res => {
+                if (!res.ok){
+                    throw Error('Sorry Ada Eror pada server')
+                }
+                return res.json()
+            })
             .then(data => {
                 SetBlogs(data)
+                SetIsPending(false)
+                SetError(null)
+            })
+            .catch(err => {
+                console.log(err.message)
+                SetError(err.message)
                 SetIsPending(false)
             })
         }, 1500)
@@ -21,6 +33,7 @@ const Home = () => {
 
     return (
         <div className="home">
+            {error && <h2> {error} </h2> }
             {isPending && <h2>Loading ...</h2>}
             {blogs && <BlogList blogs={blogs} title="All Post!" />}
         </div>

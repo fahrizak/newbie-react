a1dc2c5c2f4c424dd61e769140b9e3e2326d4f4c
fahrizak <webdaata@gmail.com>
2024-02-13 10:54:15 +0700

    Conditional Loading Message

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,15 +4,24 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, SetBlogs] = useState(null)
+    const [isPending, SetIsPending] = useState(true)
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => res.json())
-            .then(data => SetBlogs(data))
+            .then(data => {
+                SetBlogs(data)
+                SetIsPending(false)
+            })
+        }, 1500)
+
     })
 
     return (
         <div className="home">
+            {isPending && <h2>Loading ...</h2>}
             {blogs && <BlogList blogs={blogs} title="All Post!" />}
         </div>
     );
